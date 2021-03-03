ac1403ec58af8cfc629b03871f0ac5a958e14ed9
André Luferat <51307379+CataBits@users.noreply.github.com>
2021-03-03 11:44:01 -0300

    Aula 03

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDbpD-7M4AafYOUJCbm44ZpPWGEQKQpfCA",
+    authDomain: "app-cg-01.firebaseapp.com",
+    projectId: "app-cg-01",
+    storageBucket: "app-cg-01.appspot.com",
+    messagingSenderId: "184247179738",
+    appId: "1:184247179738:web:6c642e394d929ea04388e3"
+  }
 };
 
 /*
